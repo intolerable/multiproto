5c9d94c4f16a43491a2e79d2c52e15d83b566b7a
Fraser Murray <fraser.m.murray@gmail.com>
2014-11-15 00:49:01 +0000

    another big update

--- src/Data/MultiProto/Protobuf/Parser.hs
+++ src/Data/MultiProto/Protobuf/Parser.hs
@@ -1,21 +1,31 @@
+{-# OPTIONS -fno-warn-unused-do-bind #-}
+
 module Data.MultiProto.Protobuf.Parser where
 
 import Control.Applicative
-import Control.Monad
 import Data.ByteString.Char8 (ByteString)
 import Data.Attoparsec.ByteString.Char8 hiding (option)
-import Data.Maybe
 import Text.Read (readMaybe)
+import Prelude hiding (Enum)
 import qualified Data.ByteString.Char8 as ByteString
 import qualified Data.Attoparsec.ByteString.Char8 as Parser
 
-data Proto = Message'
-           | Extend'
-           | Enum ByteString [EnumInner]
-           | Import ByteString
-           | Package'
-           | Option' Option
+data Proto = ProtoMessage Message
+           | ProtoExtend
+           | ProtoEnum Enum
+           | ProtoImport ByteString
+           | ProtoPackage ByteString
+           | ProtoOption Option
            | Empty
+  deriving (Show, Read, Eq)
+
+data Message = Message ByteString [MessageInner]
+  deriving (Show, Read, Eq)
+
+data MessageInner = MessageField Field
+  deriving (Show, Read, Eq)
+
+data Field = Field
   deriving (Show, Read, Eq)
 
 data Option = Option [ByteString] Literal
@@ -24,6 +34,10 @@ data Option = Option [ByteString] Literal
 data Literal = Identifier ByteString
              | StringLit ByteString
              | IntegerLit Integer
+             | BoolLit Bool
+  deriving (Show, Read, Eq)
+
+data Enum = Enum ByteString [EnumInner]
   deriving (Show, Read, Eq)
 
 data EnumInner = EnumOption Option
@@ -31,16 +45,45 @@ data EnumInner = EnumOption Option
   deriving (Show, Read, Eq)
 
 protos :: Parser [Proto]
-protos = (proto `sepBy` skipSpace) <* endOfInput
+protos = label "protos" $
+  many proto <* endOfInput
 
 proto :: Parser Proto
-proto = import_
+proto = label "proto" $
+  comment <|>
+  (ProtoMessage <$> message) <|>
+  extend <|>
+  (ProtoEnum <$> enum) <|>
+  import_ <|>
+  package <|>
+  (ProtoOption <$> option) <|>
+  (";" *> pure Empty)
 
-message :: Parser Proto
-message = pure Empty
+comment :: Parser Proto
+comment = label "comment" $ do
+  "//"
+  takeTill (inClass "\r\n")
+  skipSpace
+  pure Empty
 
-enum :: Parser Proto
-enum = do
+message :: Parser Message
+message = label "message" $ do
+  "message"
+  skipSpace
+  iden <- identifier
+  skipSpace
+  "{"
+  skipSpace
+  res <- messageBody
+  skipSpace
+  "}"
+  return $ Message iden res
+
+messageBody :: Parser [MessageInner]
+messageBody = return []
+
+enum :: Parser Enum
+enum = label "enum" $ do
   "enum"
   skipSpace
   res <- identifier
@@ -54,7 +97,7 @@ enum = do
   return $ Enum res fields
 
 enumField :: Parser EnumInner
-enumField = do
+enumField = label "enumField" $ do
   name <- identifier
   skipSpace
   "="
@@ -65,35 +108,42 @@ enumField = do
   return $ EnumField num name
 
 identifier :: Parser ByteString
-identifier = do
+identifier = label "identifier" $ do
   c <- satisfy (inClass "a-zA-z_")
   rest <- Parser.takeWhile (inClass "a-zA-Z0-9_")
   return $ ByteString.cons c rest
 
-camelIdentifier :: Parser ByteString
-camelIdentifier = do
-  c <- satisfy (inClass "A-z")
+capitalIdentifier :: Parser ByteString
+capitalIdentifier = label "capitalIdentifier" $ do
+  c <- satisfy (inClass "A-Z")
   rest <- Parser.takeWhile (inClass "a-zA-Z0-9_")
   return $ ByteString.cons c rest
 
 extend :: Parser Proto
-extend = pure Empty
+extend = undefined
 
 import_ :: Parser Proto
-import_ = Import <$> do
+import_ = label "import_" $ do
   "import"
   skipSpace
   res <- stringLit
   skipSpace
   ";"
   skipSpace
-  return res
+  return $ ProtoImport res
 
 package :: Parser Proto
-package = pure Empty
+package = label "package" $ do
+  "package"
+  skipSpace
+  res <- stringLit
+  skipSpace
+  ";"
+  skipSpace
+  return $ ProtoPackage res
 
 option :: Parser Option
-option = do
+option = label "option" $ do
   "option"
   skipSpace
   ids <- identifier `sepBy` "."
@@ -107,25 +157,36 @@ option = do
   return $ Option ids res
 
 constant :: Parser Literal
-constant =
+constant = label "constant" $
   (Identifier <$> identifier) <|>
   (IntegerLit <$> integerLit) <|>
-  (StringLit <$> stringLit)
+  (StringLit <$> stringLit) <|>
+  (BoolLit <$> boolLit)
 
 stringLit :: Parser ByteString
-stringLit = "\"" *> takeTill (=='"') <* "\""
-
-whitespace :: Parser ()
-whitespace = " " >> pure ()
+stringLit = label "stringLit" $
+  ("\"" *> takeTill (=='"') <* "\"") <|>
+  ("'" *> takeTill (=='\'') <* "'")
 
 integerLit :: Parser Integer
-integerLit = do
-  digits <- Parser.takeWhile isDigit
-  skipSpace
-  readParse digits
+integerLit = label "integerLit" $
+  readParse =<< Parser.takeWhile (inClass "0-9.")
+
+hexLit :: Parser Integer
+hexLit = label "hexLit" $ do
+  front <- "0x" <|> "0X"
+  digits <- Parser.takeWhile (inClass "A-Fa-f0-9")
+  readParse $ ByteString.append front digits
+
+boolLit :: Parser Bool
+boolLit = ("true" *> pure True) <|>
+          ("false" *> pure False)
 
 readParse :: Read a => ByteString -> Parser a
 readParse s =
   case readMaybe $ ByteString.unpack s of
     Just res -> pure res
     Nothing -> fail "no read"
+
+label :: String -> Parser a -> Parser a
+label n p = p <?> n
--- test/Data/MultiProto/Protobuf/ParserSpec.hs
+++ test/Data/MultiProto/Protobuf/ParserSpec.hs
@@ -35,6 +35,50 @@ spec = do
       p "h_ello" `shouldBe` Right "h_ello"
       p "_hello" `shouldBe` Right "_hello"
 
+  describe "capitalIdentifier" $ do
+    let p = parseOnly capitalIdentifier
+
+    it "should be able to parse identifiers w/ leading capitals" $ do
+      p "Hello" `shouldBe` Right "Hello"
+      p "HELLO" `shouldBe` Right "HELLO"
+
+    it "should fail for identifiers w/o leading caps" $ do
+      p "hello" `shouldSatisfy` isLeft
+
+  describe "integerLit" $ do
+    let p = parseOnly integerLit
+
+    it "should be able to parse integer literals" $ do
+      p "5" `shouldBe` Right 5
+
+    it "should fail for things that aren't integers" $ do
+      p "5.5" `shouldSatisfy` isLeft
+      p "no" `shouldSatisfy` isLeft
+
+  describe "hexLit" $ do
+    let p = parseOnly hexLit
+
+    it "should be able to parse hex literals" $ do
+      p "0xff" `shouldBe` Right 0xff
+      p "0X0" `shouldBe` Right 0x0
+      p "0xDEADBEEF" `shouldBe` Right 0xDEADBEEF
+
+    it "should fail on invalid hex literals" $ do
+      p "0xno" `shouldSatisfy` isLeft
+      p "0x" `shouldSatisfy` isLeft
+
+  describe "boolLit" $ do
+    let p = parseOnly boolLit
+
+    it "should be able to parse boolean literals" $ do
+      p "true" `shouldBe` Right True
+      p "false" `shouldBe` Right False
+
+    it "should fail for things that aren't boolean literals" $ do
+      p "True" `shouldSatisfy` isLeft
+      p "FALSE" `shouldSatisfy` isLeft
+      p "0" `shouldSatisfy` isLeft
+
 isLeft :: Either a b -> Bool
 isLeft (Left _) = True
 isLeft _ = False
