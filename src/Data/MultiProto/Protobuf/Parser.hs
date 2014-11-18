3665e62cb7a04ebc4537debb9c611ae8272fc46a
Fraser Murray <fraser.m.murray@gmail.com>
2014-11-18 15:55:13 +0000

    neater

--- src/Data/MultiProto/Protobuf/Parser.hs
+++ src/Data/MultiProto/Protobuf/Parser.hs
@@ -84,93 +84,74 @@ protos :: Parser [Proto]
 protos = many proto <* endOfInput
 
 proto :: Parser Proto
-proto = label "proto" $
-  lineComment <|>
-  blockComment <|>
-  (ProtoMessage <$> message) <|>
-  (ProtoExtend <$> extend) <|>
-  (ProtoEnum <$> enum) <|>
-  (ProtoImport <$> import_) <|>
-  (ProtoPackage <$> package) <|>
-  (ProtoOption <$> option) <|>
-  (";" *> pure Empty)
+proto = choice
+  [ lineComment *> pure Empty
+  , blockComment *> pure Empty
+  , ProtoMessage <$> message
+  , ProtoExtend <$> extend
+  , ProtoEnum <$> enum
+  , ProtoImport <$> import_
+  , ProtoPackage <$> package
+  , ProtoOption <$> option
+  , ";" *> pure Empty ]
 
-lineComment :: Parser Proto
-lineComment = "//" *> ss (takeTill (inClass "\r\n")) *> pure Empty
+lineComment :: Parser ByteString
+lineComment = "//" *> ss (takeTill (inClass "\r\n"))
 
-blockComment :: Parser Proto
-blockComment = (ss ("/*" *> manyTill anyChar "*/") *> pure Empty)
+blockComment :: Parser ByteString
+blockComment = ByteString.pack <$> ss ("/*" *> manyTill anyChar "*/")
 
 message :: Parser Message
 message = ss "message" *> (Message <$> ss identifier <*> ss messageBody)
 
 messageBody :: Parser [MessageInner]
-messageBody = label "messageBody" $ do
-  ss "{"
-  inners <- ss $ many $
-    (MessageField <$> messageField) <|>
-    (MessageEnum <$> enum) <|>
-    (NestedMessage <$> message) <|>
-    (MessageExtend <$> extend)
-  ss "}"
-  return inners
+messageBody = wrapped (ss "{") (ss "}") $ ss $ many $ choice
+  [ MessageField <$> messageField
+  , MessageEnum <$> enum
+  , NestedMessage <$> message
+  , MessageExtend <$> extend ]
 
 messageField :: Parser Field
-messageField = do
-  m <- ss modifier
-  t <- ss type_
-  i <- ss identifier
-  ss "="
-  n <- ss integerLit
-  opts <- ss $ Parser.option [] $ do
-    ss "["
-    res <- ss optionBody `sepBy1` ","
-    ss "]"
-    return res
-  ss ";"
-  return $ Field m t i n opts
+messageField = Field <$> ss modifier
+                     <*> ss type_
+                     <*> ss identifier <* ss "="
+                     <*> ss integerLit
+                     <*> ss options <* ss ";"
+
+options :: Parser [Option]
+options = Parser.option [] $ wrapped (ss "[") (ss "]") $ ss optionBody `sepBy1` ","
 
 optionBody :: Parser Option
 optionBody = Option <$> ss (identifier `sepBy1` ".") <*> (ss "=" *> constant)
 
 modifier :: Parser Modifier
-modifier = ("required" *> pure Required) <|> ("optional" *> pure Optional) <|> ("repeated" *> pure Repeated)
+modifier = choice ["required" *> pure Required, "optional" *> pure Optional, "repeated" *> pure Repeated]
 
 type_ :: Parser Type
-type_ =
-  ("double" *> pure TDouble) <|>
-  ("float" *> pure TFloat) <|>
-  ("int32" *> pure TInt32) <|>
-  ("int64" *> pure TInt64) <|>
-  ("uint32" *> pure TUInt32) <|>
-  ("uint64" *> pure TUInt64) <|>
-  ("sint32" *> pure TSInt32) <|>
-  ("sint64" *> pure TSInt64) <|>
-  ("fixed32" *> pure TFixed32) <|>
-  ("sfixed32" *> pure TSFixed32) <|>
-  ("sfixed64" *> pure TSFixed64) <|>
-  ("bool" *> pure TBool) <|>
-  ("string" *> pure TString) <|>
-  ("bytes" *> pure TBytes) <|>
-  (TUser <$> userType)
+type_ = choice
+  [ "double" *> pure TDouble
+  , "float" *> pure TFloat
+  , "int32" *> pure TInt32
+  , "int64" *> pure TInt64
+  , "uint32" *> pure TUInt32
+  , "uint64" *> pure TUInt64
+  , "sint32" *> pure TSInt32
+  , "sint64" *> pure TSInt64
+  , "fixed32" *> pure TFixed32
+  , "sfixed32" *> pure TSFixed32
+  , "sfixed64" *> pure TSFixed64
+  , "bool" *> pure TBool
+  , "string" *> pure TString
+  , "bytes" *> pure TBytes
+  , TUser <$> userType ]
 
 userType :: Parser UserType
-userType = do
-  qualified <- ss $ Parser.option False ("." *> pure True)
-  ids <- identifier `sepBy` "."
-  return $
-    if qualified
-      then FullyQualified ids
-      else InScope ids
+userType = (FullyQualified <$> ("." *> ids)) <|> (InScope <$> ids)
+  where ids = identifier `sepBy1` "."
 
 enum :: Parser Enum
-enum = label "enum" $ do
-  ss "enum"
-  res <- ss identifier
-  ss "{"
-  fields <- ss $ many enumField
-  ss "}"
-  return $ Enum res fields
+enum = Enum <$> (ss "enum" *> ss identifier)
+            <*> wrapped (ss "{") (ss "}") (ss $ many enumField)
 
 enumField :: Parser EnumInner
 enumField = EnumField <$> (ss identifier <* ss "=") <*> (ss integerLit <* ss ";")
@@ -194,7 +175,7 @@ option :: Parser Option
 option = Option <$> (ss "option" *> ss (identifier `sepBy` ".")) <*> (ss "=" *> ss constant <* ss ";")
 
 constant :: Parser Literal
-constant = choice $
+constant = choice
   [ Identifier <$> identifier
   , IntegerLit <$> integerLit
   , FloatLit <$> floatLit
@@ -202,7 +183,7 @@ constant = choice $
   , BoolLit <$> boolLit ]
 
 stringLit :: Parser ByteString
-stringLit = label "stringLit" $
+stringLit =
   ("\"" *> takeTill (=='"') <* "\"") <|>
   ("'" *> takeTill (=='\'') <* "'")
 
@@ -220,12 +201,10 @@ floatLit = (=<<) (readParse . fst) $ match $ do
     Parser.option "" $ "+" <|> "-"
     many1 digit
 
-
 hexLit :: Parser Integer
-hexLit = label "hexLit" $ do
-  front <- "0x" <|> "0X"
-  digits <- Parser.takeWhile (inClass "A-Fa-f0-9")
-  readParse $ ByteString.append front digits
+hexLit = (=<<) (readParse . fst) $ match $ do
+  "0x" <|> "0X"
+  Parser.takeWhile (inClass "A-Fa-f0-9")
 
 boolLit :: Parser Bool
 boolLit = ("true" *> pure True) <|> ("false" *> pure False)
@@ -236,8 +215,8 @@ readParse s =
     Just res -> pure res
     Nothing -> fail "no read"
 
-label :: String -> Parser a -> Parser a
-label n p = p <?> n
-
 ss :: Parser a -> Parser a
 ss p = p <* skipSpace
+
+wrapped :: Parser a -> Parser b -> Parser c -> Parser c
+wrapped begin end middle = begin *> middle <* end
